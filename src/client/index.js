ee7f8bdcb04532d12a46dc28115d2f4072d7ef67
Stephen Moss <bladexr8@gmail.com>
2022-04-30 22:29:04 +1000

    Hello World UI!

---
+++ src/client/index.js
@@ -0,0 +1,6 @@
+// main starting point of our frontend code
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
