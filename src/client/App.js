219b7d882c8063a21f5928cc4475b84ffcc3cca6
Stephen Moss <bladexr8@gmail.com>
2022-05-01 06:27:49 +1000

    first cut React UI

--- src/client/App.js
+++ src/client/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Helmet } from 'react-helmet';
 import '../../assets/css/style.css'
 
 const initialPosts = [
@@ -24,10 +25,41 @@ const initialPosts = [
 const App = () => {
   // set initial state
   const [posts, setPosts] = useState(initialPosts);
+  const [postContent, setPostContent] = useState('');
+
+  // form handler
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('handleSubmit...');
+    const newPost = {
+      id: posts.length + 1,
+      text: postContent,
+      user: {
+        avatar: '/uploads/avatar1.png',
+        username: 'Fake User'
+      }
+    };
+    console.log(newPost);
+    // update state
+    setPosts([newPost, ...posts]);
+    setPostContent('');
+  }
+
   return (
     <div className="container">
+      <Helmet>
+        <title>Graphbook - Feed</title>
+        <meta name="description" content="Newsfeed of all your friends on Graphbook" />
+      </Helmet>
+      <div className='postForm'>
+        <form onSubmit={handleSubmit}>
+          <textarea value={postContent} onChange={(e) => setPostContent(e.target.value)}
+            placeholder="Write your custom post..." />
+        <input type="submit" value="Submit" />
+        </form>
+      </div>
       <div className="feed">
-        { initialPosts.map((post, i) => 
+        { posts.map((post, i) => 
           <div key={post.id} className="post">
             <div className="header">
               <img src={post.user.avatar} />
